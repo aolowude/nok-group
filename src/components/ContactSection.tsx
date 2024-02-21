0886cd301e74fe35071e851aee1bad75976cef18
aolowude <aolowude@gmail.com>
2024-02-21 17:56:30 +0000

    Mobile contact us

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -9,7 +9,7 @@ export function ContactSection() {
       <FadeIn className="-mx-6 rounded-4xl bg-neutral-950 px-6 py-20 sm:mx-0 sm:py-32 md:px-12">
         <div className="mx-auto max-w-4xl">
           <div className="flex justify-between">
-            <h2 className="font-display text-3xl font-medium text-white [text-wrap:balance] sm:text-4xl">
+            <h2 className="font-display text-xl font-medium text-white [text-wrap:balance] sm:text-4xl">
               Get in touch with us
             </h2>
             <div className="">
