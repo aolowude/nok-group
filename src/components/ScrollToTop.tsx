6a192f7f0eb9c787a40ae1382c828edc30ab7bc7
aolowude <aolowude@gmail.com>
2024-02-22 18:15:54 +0000

    Back to top green

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -38,7 +38,7 @@ const ScrollToTopButton = () => {
       <button
         ref={buttonRef}
         onClick={handleClick}
-        className="border-teal text-teal invisible fixed bottom-0 right-0 mb-8 mr-8 rounded-full border bg-white p-4 opacity-0 shadow-md transition duration-200 ease-in-out hover:bg-opacity-90"
+        className="border-teal text-teal scrollToTopBg invisible fixed bottom-0 right-0 mb-8 mr-8 rounded-full p-4 opacity-0 shadow-md transition duration-200 ease-in-out hover:bg-opacity-90"
       >
         <Image src={ScrollToTopIcon} width={20} alt="Scroll To Top Button" />
       </button>
